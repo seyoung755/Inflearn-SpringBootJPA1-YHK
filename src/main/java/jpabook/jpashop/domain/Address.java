855ff5a627463645dfc6cf7e06f6bc2c9750ad32
seyeong <seyoung7555@naver.com>
2022-02-07 00:40:14 +0900

    값 타입 제한조건 설정
    - Immutable 하도록 생성자를 생성하고 protected 생성자 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,16 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // JPA 스펙 상 기본 생성자가 필요하므로 만들어주되, protected로 닫아둔다.
+    // JPA 구현 라이브러리가 리플렉션 같은 기술을 사용할 수 있도록 지원하는 것이다.
+    protected Address() {
+    }
+
+    // 값 타입은 변경 불가능하도록 설계해야 하므로 객체 생성 시에만 값을 할당하고 변경 불가능하게 한다.
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
