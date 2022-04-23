6067cd9e88ed8fea7986b2ac1087b4735212e472
seyeong <seyoung7555@naver.com>
2022-04-23 17:45:11 +0900

    test: 회원 도메인 테스트 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+//        em.flush(); // @Transactional로 인해 commit하지 않고 roll-back 하므로 insert 쿼리가 발생하지 않는다. flush를 통해 강제적으로 commit
+        assertThat(member).isEqualTo(memberRepository.findOne(savedId)); // 같은 영속성 컨텍스트에선 동일한 객체가 나온다.
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        Assertions.assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2));
+    }
+}
