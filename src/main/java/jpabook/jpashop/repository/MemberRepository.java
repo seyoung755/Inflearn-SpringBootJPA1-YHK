41302c8e145debc58db6a040731687acb77be917
seyeong <seyoung7555@naver.com>
2022-04-23 17:22:35 +0900

    feat: MemberService 구현

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,17 +1,21 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-
-    @PersistenceContext
-    private EntityManager em; // Spring이 객체를 만들어 주입해준다.
+    /*
+    Spring이 객체를 만들어 주입해준다.
+    원래는 @PersistContext를 달아줘야되는데, 최신 스프링부트에서는 @Autowired를 통해서도 가능해진다.
+    그래서 아래와 같이 생성자 주입이 가능해진다.
+     */
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
